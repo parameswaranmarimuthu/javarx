67550db19d2aba1aa1f0eb51f1531c0a55f8f47e
Parameswaran Marimuthu <parameswaran.marimuthu@macys.com>
2017-10-26 11:45:03 -0700

    added more classes

---
+++ src/test/java/AggregateTest.java
@@ -0,0 +1,15 @@
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import org.junit.Test;
+
+public class AggregateTest {
+
+    @Test
+    public void testingReduce() throws Exception {
+        Maybe<Integer> reduce = Observable.range(1, 5).reduce((total, next) -> {
+            System.out.format("Total: %d, Next: %d\n", total, next);
+            return total * next;
+        });
+        System.out.println(reduce.defaultIfEmpty(-1).blockingGet());
+    }
+}
